c5f95a2603ab70b30cbb5dd9b9c745b518d6ad7c
Faith nkatha <faithnkatha160@gmail.com>
2023-04-23 19:41:00 +0300

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,50 @@
+/*NUMBER
+let message="hello";
+message="123456";
+alert(message);//at a moment a variable can store string then number.
+ BIGINT
+ in javascript youcan write number greater than 2^53 -1 and less than -2^52-1
+bigint was added to rep integers of abritary length.
+bigint is created by adding n at the end of integer.
+STRING
+ 1.Must be surrounded by quotes
+    3 types of quotes
+    "" double quotes
+    '' singlequotes 
+    `` backticks- extented functionality -allow us to embed
+     variables and expression into a string by wrapping them in
+ ${} can only be done  in backticks
+ 
+ let name ="john";
+ alert(`hello ${name}!`) //embed variable
+
+ alert(`the result is ${2+8}`);//embed expression
+ BOOLEAN
+ let isGreater = 4 > 10;
+ alert(isGreater);
+ NULL
+ it contains only  null value
+ in javascriptits not refence to non existing object but value unknown or empty
+let age= null;
+UNDEFINED
+Value is not assigned 
+variable is declared but not assigned.
+let age;
+alert(age);
+OBJECT(UNPRIMITIVE)
+object type is special.
+object store collections of data and more complex entities
+SYMBOL
+is used to create unique identifiers for objects.*/
+TYPEOF
+Return type of the operand
+typeof unddefined
+typeof 0
+typeof 10n
+typeof "okay"
+typeof symbol("id")
+typeof math
+typeof null
+typeof alert
+
+
