8f9d94241861ed346bacc3f1579fe5f50e43ecf3
Faith nkatha <faithnkatha160@gmail.com>
2023-05-03 21:09:15 +0300

    Add files via upload

---
+++ interactions.js
@@ -0,0 +1,36 @@
+/*INTERATIONS: ALERT, PROMPT, CONFIRM.
+alert
+ it interact with the user by showing a message and wait for user to press okay 
+ alert("click ok");
+PROMPT
+result=prompt(title,[default]) ;
+title- the text to show the visitor
+default-optional initial value for input field
+let age= prompt("how old are you?",35);
+alert(`you are ${age} old`);
+CONFIRM
+shows message and waits for user to press ok or cancel
+Result= confirm(question);
+let isBoss =confirm("are the boss?");
+alert(isBoss);
+all this methods are modal and dont allow the visitor to interact with rest of page til dismissed.
+
+task one
+alert("swahili version!!");
+let name=prompt("jina lako?", );
+alert(`jina langu ${name}.asante!`);
+task two
+let name= prompt("jina lako?");
+let age=prompt("umri wako?");
+
+if (age<18) {
+	alert(` Habari ${name}, jaribu tena baada ya miaka michache!`);
+}
+	else{
+		alert(`Welcome! ${name}.Wewe kwa sasa ${age} miaka.`);
+		
+	}
+	*/
+
+alert("mambo");
+
---
+++ swahili.js
@@ -0,0 +1,8 @@
+let jina= prompt("Jina Lako?", );
+let umri= prompt("umri wako?", );
+if (umri<18){
+alert(`Mambo ${jina}, Jaribu tena baada ya miaka chache😔`);
+}
+else{
+	alert(`Mambo ${jina} ,Karibu uko na umri wa miaka ${umri} 😊`)
+}
